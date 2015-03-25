ea603fd35bc8eb2c93c84c62328e1bdffd822d7d
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 17:55:48 -0400

    How to implement a webview in Xamarin iOS.

---
+++ WebView.iOSViewController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using WebKit;
+using WebView;
+
+using Foundation;
+using UIKit;
+
+namespace WebView.iOS
+{
+	public partial class WebView_iOSViewController : UIViewController
+	{
+
+
+		string url = "http://xamarin.com";
+
+		//View.AddSubview(webView);
+
+
+
+		public WebView_iOSViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			webview.LoadRequest (new NSUrlRequest(new NSUrl(url)));
+
+			webview.ScalesPageToFit = true;
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
